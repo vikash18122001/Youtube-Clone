d578c8d1a1577b3317352a25f280e9cafd940bf5
Ayush Dwivedi <amayush0818@gmail.com>
2022-12-17 11:30:48 +0530

    Channel Changed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,7 +16,7 @@ export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
-  { name: 'JS Mastery', icon: <CodeIcon />, },
+  { name: 'take U forward', icon: <CodeIcon />, },
   { name: 'Coding', icon: <CodeIcon />, },
   { name: 'ReactJS', icon: <CodeIcon />, },
   { name: 'NextJS', icon: <CodeIcon />, },
@@ -35,8 +35,8 @@ export const categories = [
 ];
 
 export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
-export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
-export const demoVideoUrl = '/video/GDa8kZLNhJ4';
-export const demoChannelTitle = 'JavaScript Mastery';
-export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoChannelUrl = '/@takeUforward';
+export const demoVideoUrl = '/watch?v=5EVGDpbuEFk';
+export const demoChannelTitle = 'take U forward';
+export const demoVideoTitle = 'Curated Resources for Advanced DSA - MAANG or PBC Interviews';
 export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
